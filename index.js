277cfa8c7cd0ccb253704cf2752aeac70a8eda2d
Šárka Princová Maroušková <sarka.marouskova@gmail.com>
2021-05-20 23:01:30 +0200

    Poladěné hamburger menu a responzivita

--- index.js
+++ index.js
@@ -4,9 +4,19 @@ console.log('funguju');
 const btnElm = document.querySelector('#nav-btn');
 const menuElm = document.querySelector('nav');
 
-const klik = () => {
+btnElm.addEventListener("click", ()=> {
+  menuElm.classList.toggle("nav-closed");
+});
+
+document.querySelectorAll(".nav-link").forEach((item)=> {
+  item.addEventListener("click", ()=> {
+    menuElm.classList.add("nav-closed");
+  });
+});
+
+/*const klik = () => {
   console.log('klikam jak blazen');
   menuElm.classList.toggle('nav-closed');
 };
 
-btnElm.addEventListener('click', klik);
+btnElm.addEventListener('click', klik);*/
